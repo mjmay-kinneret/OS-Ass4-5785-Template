c71d3ef88dcb1d4c09a20410a0ce1393080e1c65
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2024-12-17 13:30:14 +0200

    Update test13-two-files-signals.sh

--- test13-two-files-signals.sh
+++ test13-two-files-signals.sh
@@ -4,7 +4,7 @@ echo Test 13: Two files top 100 words and 3 Ctrl C
 ./longestwords 100 tests/moby-dick-sentences.txt tests/pride-sentences.txt  > outfiles/test13-moby-pride-signal.txt 2>outfiles/test13-words-so-far&
 
 for i in {1..3}; do
-  sleep 2
+  sleep 1
   kill -SIGINT $!
   echo Sent Ctrl+C signal
 done
