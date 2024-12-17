f5fbfa425ed7e90e4e24df4e480e16614549e4cf
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2024-12-17 13:30:28 +0200

    Update test14-four-files-signals.sh

--- test14-four-files-signals.sh
+++ test14-four-files-signals.sh
@@ -4,7 +4,7 @@ echo Run Test 14: Four files and 5 Ctrl C
 ./longestwords 150 tests/moby-dick-sentences.txt tests/pride-sentences.txt tests/northanger-abbey-sentences.txt tests/frankenstein-sentences.txt > outfiles/test14-moby-pride-northanger-frankenstein-signal.txt 2>outfiles/test14-words-so-far&
 
 for i in {1..5}; do
-  sleep 2
+  sleep 1
   kill -SIGINT $!
   echo Sent Ctrl+C signal
 done
