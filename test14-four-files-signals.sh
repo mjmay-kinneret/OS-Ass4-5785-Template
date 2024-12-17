6a523d231c3c4379cd3491688712449bcea023d3
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2024-12-17 12:29:39 +0200

    Update test14-four-files-signals.sh

--- test14-four-files-signals.sh
+++ test14-four-files-signals.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Run Test 14: Four files and 5 Ctrl C
-./longestwords 150 tests/moby-dick-sentences.txt tests/pride-sentences.txt tests/northanger-abbey-sentences.txt tests/frankenstein-sentences.txt > outfiles/test14-moby-pride-northanger-frankenstein-50.txt 2>outfiles/test14-words-so-far&
+./longestwords 150 tests/moby-dick-sentences.txt tests/pride-sentences.txt tests/northanger-abbey-sentences.txt tests/frankenstein-sentences.txt > outfiles/test14-moby-pride-northanger-frankenstein-signal.txt 2>outfiles/test14-words-so-far&
 
 for i in {1..5}; do
   sleep 2
